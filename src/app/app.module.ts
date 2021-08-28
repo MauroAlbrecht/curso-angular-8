50a745324da9b535b0452d5b259bd9726c01f122
MauroAlbrecht <mauroint@gmail.com>
2021-08-28 16:06:22 -0300

    adicionado navbar e controle de rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,31 @@ import {AppComponent} from './app.component';
 import {CourseListComponent} from './courses/course-list.component';
 import {StarComponent} from './star/star.component';
 import {ReplacePipe} from './pipe/replace.pipe';
+import {NavBarComponent} from './nav-bar/nav-bar.component';
+import {RouterModule} from '@angular/router';
+import {Error404Compoennt} from './error-404/error-404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
     StarComponent,
-    ReplacePipe
+    ReplacePipe,
+    NavBarComponent,
+    Error404Compoennt
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      }, {
+        path: 'courses', component: CourseListComponent
+      }, {
+        path: '**', component: Error404Compoennt
+      }
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
