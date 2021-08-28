84cfd995191a26b5e5f9a8865e5452da73a9749e
MauroAlbrecht <mauroint@gmail.com>
2021-08-28 11:02:01 -0300

    adicionado listagem de cursos

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,9 +1,37 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Course} from './course';
 
 @Component({
   selector: 'app-course-list',
   templateUrl: './course-list.component.html'
 })
-export class CourseListComponent {
+export class CourseListComponent implements OnInit {
 
+  // inicia vazio
+  courses: Course[] = [];
+
+  ngOnInit(): void {
+    this.courses = [
+      {
+        id: 1,
+        name: 'JAVA',
+        imageUrl: '',
+        price: 20.30,
+        code: '102',
+        duration: 8,
+        rating: 2,
+        relaseDate: 'November, 2, 2021'
+      },
+      {
+        id: 2,
+        name: 'Thymeleaf',
+        imageUrl: '',
+        price: 20.30,
+        code: 'J8a8sj',
+        duration: 4,
+        rating: 23,
+        relaseDate: 'November, 4, 2021'
+      }
+    ];
+  }
 }
