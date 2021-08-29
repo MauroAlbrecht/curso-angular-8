ca215c766777221095ef90298a48e0cb896073ef
MauroAlbrecht <mauroint@gmail.com>
2021-08-28 21:53:26 -0300

    ajustes de fonte separando módulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,15 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {AppComponent} from './app.component';
-import {NavBarComponent} from './nav-bar/nav-bar.component';
 import {RouterModule} from '@angular/router';
 import {Error404Compoennt} from './error-404/error-404.component';
 import {HttpClientModule} from '@angular/common/http';
 import {CourseModule} from './courses/course.module';
+import {CoreModule} from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
     Error404Compoennt
   ],
   imports: [// aqui importa os módulos
@@ -19,6 +18,7 @@ import {CourseModule} from './courses/course.module';
     FormsModule,
     HttpClientModule,
     CourseModule,
+    CoreModule,
     RouterModule.forRoot([
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
--- src/app/courses/course.module.ts
+++ src/app/courses/course.module.ts
@@ -4,25 +4,20 @@ import {CourseInfoComponent} from './course-info.component';
 import {RouterModule} from '@angular/router';
 import {FormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
-import {ReplacePipe} from '../pipe/replace.pipe';
 import {StarModule} from '../shared/component/star/star.module';
-
-/*import {StarModule} from '../shared/component/star/star.module';
-import {AppPipeModule} from '../shared/pipe/app-pipe.module';*/
+import {AppPipeModule} from '../shared/pipe/app-pipe.module';
 
 @NgModule({
   declarations: [
     CourseListComponent,
-    CourseInfoComponent,
-    ReplacePipe
+    CourseInfoComponent
   ],
   imports: [
     CommonModule,
     FormsModule,
     CommonModule,
     StarModule,
-    /*  StarModule,
-      AppPipeModule,*/
+    AppPipeModule,
     RouterModule.forChild([
       {
         path: 'courses', component: CourseListComponent
