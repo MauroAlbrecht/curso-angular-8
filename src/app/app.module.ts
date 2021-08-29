6fd7f4e08a1fd91a35c1d2ed907cb282400f5036
MauroAlbrecht <mauroint@gmail.com>
2021-08-28 22:20:27 -0300

    finalizando projeto, organizando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {AppComponent} from './app.component';
 import {RouterModule} from '@angular/router';
-import {Error404Compoennt} from './error-404/error-404.component';
 import {HttpClientModule} from '@angular/common/http';
 import {CourseModule} from './courses/course.module';
 import {CoreModule} from './core/core.module';
@@ -11,7 +10,6 @@ import {CoreModule} from './core/core.module';
 @NgModule({
   declarations: [
     AppComponent,
-    Error404Compoennt
   ],
   imports: [// aqui importa os módulos
     BrowserModule,
@@ -22,9 +20,7 @@ import {CoreModule} from './core/core.module';
     RouterModule.forRoot([
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
-      }, {
-        path: '**', component: Error404Compoennt
-      }
+      },
     ]),
   ],
   providers: [],
